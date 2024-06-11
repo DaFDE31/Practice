e442331a9cdba611bf9d3bd94c8580f405e9dd5e
Danielson A <99297691+DaFDE31@users.noreply.github.com>
2024-06-11 12:51:12 -0400

    Update LinkedList

--- src/main/java/problems/LinkedList.java
+++ src/main/java/problems/LinkedList.java
@@ -1,23 +1,28 @@
 package problems;
 
 class Node<G>{
-  G data;
-  Node nextNode;
-  public Node<G>(){}
-  public Node<G>(G data){this.data = data;}
-  public Node<G>(G data, Node nextNode){
+  private G data;// The data and value held inside the node
+  private Node nextNode; // The Node that the current one is pointing to
+  public Node(){}
+  public Node(G data){this.data = data;}
+  public Node(G data, Node nextNode){
       this.data = data;
       this.nextNode = nextNode;
   }
+
+  //Make getters and setters.
 }
 public class LinkedList<G>{
-  Node<G> headNode;
-  public LinkedList<G> (){}
-  public LinkedList<G> (Node n){
+  private Node<G> headNode;
+  public LinkedList (){}
+  public LinkedList (Node n){
     headNode = n;
   }
+  public LinkedList (G n){
+    headNode.setData(n);
+  }
 
-  public void insert(Node newNode){
+  private void insertNode(Node newNode){
     
   }
 
@@ -29,4 +34,34 @@ public class LinkedList<G>{
       
     }
   }
+
+  public void insertAtHead(G newData){
+      headNode = new Node(newData, headNode);
+  }
+  
+  public void insertAtEnd(Node newNode){
+    if (head == null){
+      head = newNode;
+    }
+    Node current = head;
+    while(current.hasNext()){
+      current = current.getNextNode();
+    }
+    current.setNextNode(newNode);
+  }
+
+  public void insertAtEnd(G data){
+    insertAtEnd(new Node(data));
+  }
+
+
+
+
+
+
+
+
+
+
+  
 }
